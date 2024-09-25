77869b0531292767ce2f2d51bd8480a97c3d54b0
Carlos <cjrencoret@miuandes.cl>
2024-09-25 19:10:58 -0300

    change role to role_id in utilizer model

--- app/models/utilizer.rb
+++ app/models/utilizer.rb
@@ -2,7 +2,13 @@ class Utilizer < ApplicationRecord
     has_many :courses, foreign_key: :teacher_id
     has_many :questions
     has_many :answers
-    validates :email, :login, :password, :name, :last_name, :role, presence: true
+    validates :email, :login, :password, :name, :last_name, :role_id, presence: true
     validates :email, uniqueness: true
+
+    enum role_id:{
+      student: 0,
+      teacher: 1
+    }
+    
   end
   
