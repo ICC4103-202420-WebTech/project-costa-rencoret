3b4677903f6eb0535591e49b35f981029e9e23b6
Jose Costa <jicosta@miuandes.cl>
2024-09-25 17:07:57 -0300

    Created basic webpage with some functions

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,14 @@
+class PagesController < ApplicationController
+    def home
+      # Logic for home page (if any)
+    end
+  
+    def about
+      # Logic for about page (if any)
+    end
+  
+    def contact
+      # Logic for contact page (if any)
+    end
+  end
+  
